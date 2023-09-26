b307b43d4f2fb34b8162c8ac187a67e81ef61e13
SeherKumsar <seherkumsar99@outlook.com>
2023-09-26 16:13:02 +0300

    template_navbar

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
 # url için Redirect
 from django.urls import reverse
+
+# from django.template.loader import render_to_string
 
 
 monthly_challenges = {
@@ -16,40 +19,53 @@ monthly_challenges = {
     "september": "Eylül",
     "october": "Ekim",
     "november": "Kasım",
-    "december": "Aralık",
+    "december": None,
 }
 
 # Create views
 
+
 def index(request):
-    list_items = ""
+    # list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-        # a href bağlantısı yazılırken tırnak hatası olmaması için \" \" kullanılır
-    # "<li><a href="...">January</a></li><li><a href="...">February</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    #     # a href bağlantısı yazılırken tırnak hatası olmaması için \" \" kullanılır
+    # # "<li><a href="...">January</a></li><li><a href="...">February</a></li>"
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
+
+    return render(request, "challenges/index.html", {"months": months})
 
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(months):
-        return HttpResponseNotFound("<h1>Invalid month</h1>")
+        # return HttpResponseNotFound("<h1>Invalid month</h1>")
+        return HttpResponseNotFound("Invalid month")
 
     redirect_month = months[month - 1]
-    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenges/january
-    return HttpResponseRedirect(redirect_month)
+    # /challenges/january
+    redirect_path = reverse("month-challenge", args=[redirect_month]) ## /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(
+            request,
+            "challenges/challenge.html",
+            #{"text": challenge_text, "month_name": month.capitalize()},
+            {"text": challenge_text, "month_name": month}
+        )
+        # response_data = render_to_string("challenges/challenge.html")
+        # response_data = f"<h1>{challenge_text}</h1>"
+        # response_data = render_to_string("challenge.html")
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
